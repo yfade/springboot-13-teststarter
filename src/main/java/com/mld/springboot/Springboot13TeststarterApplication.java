bc81ba46b9a881af968e59d9abe414c29c787803
daibiao <db_work@126.com>
2021-01-25 22:16:48 +0800

    init

---
+++ src/main/java/com/mld/springboot/Springboot13TeststarterApplication.java
@@ -0,0 +1,13 @@
+package com.mld.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot13TeststarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot13TeststarterApplication.class, args);
+    }
+
+}
